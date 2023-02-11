72cfd510b11c98075965d50e6ba1c4a5eff0d56e
jpcoolguy777 <123331288+jpcoolguy777@users.noreply.github.com>
2023-02-11 12:12:27 +0100

    Update Form1.vb

--- Form1.vb
+++ Form1.vb
@@ -6,26 +6,40 @@
 
     Private Sub add_Click(sender As Object, e As EventArgs) Handles add.Click
         OP = 1
+        Label3.Text = "Number 1"
+        Label2.Text = "Number 2"
     End Sub
 
 
     Private Sub Subtract_Click(sender As Object, e As EventArgs) Handles Subtract.Click
         OP = 2
+        Label3.Text = "Number 1"
+        Label2.Text = "Number 2"
     End Sub
 
     Private Sub Multiply_Click(sender As Object, e As EventArgs) Handles Multiply.Click
         OP = 3
+        Label3.Text = "Number 1"
+        Label2.Text = "Number 2"
     End Sub
 
     Private Sub Divide_Click(sender As Object, e As EventArgs) Handles Divide.Click
         OP = 4
+        Label3.Text = "Number 1"
+        Label2.Text = "Number 2"
     End Sub
 
     Private Sub square_Click(sender As Object, e As EventArgs) Handles square.Click
         OP = 5
+        Label3.Text = "To the power of"
+        Label2.Text = "Desired number"
     End Sub
 
     Private Sub equals_Click(sender As Object, e As EventArgs) Handles equals.Click
+        Label3.Text = "Number 1"
+        Label2.Text = "Number 2"
+        
+        
         num1 = input1.Text
         num2 = input2.Text
 
